e734961d2ec53e84570db0630a74990d2ae32105
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2022-06-10 22:36:41 +0600

    Solve daily challenge - June 06, 2022

---
+++ src/30_days/2022/june/160_intersection_of_two_linked_lists.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def get_len(self, head):
+        count = 0
+        while head:
+            head = head.next
+            count += 1
+
+        return count
+
+    def skip_nodes(self, head, skip_amount):
+        for _ in range(skip_amount):
+            head = head.next
+
+        return head
+
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        lena = self.get_len(headA)
+        lenb = self.get_len(headB)
+
+        if lena > lenb:
+            headA = self.skip_nodes(headA, lena - lenb)
+        elif lenb > lena:
+            headB = self.skip_nodes(headB, lenb - lena)
+
+        while headA.next:
+            if headA is headB:
+                return headB
+
+            headA = headA.next
+            headB = headB.next
+
+        return None
