26d6f8717e2eab92a498e0ab7f716d59e251e5e3
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2022-12-25 14:29:50 +0600

    1523. Count Odd Numbers in an Interval Range (Easy)

---
+++ src/problems_all/1523_count_odd_numbers_in_an_interval_range.py
@@ -0,0 +1,35 @@
+"""
+https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/?envType=study-plan&id=programming-skills-i
+
+Runtime: 34 ms
+Memory: 13.9 MB
+"""
+
+
+class Solution:
+    """
+    The number of odd numbers between 0 and n is n // 2 where n is even
+    So we promote `high` to the next even number and downgrade low to 
+    the previous even number.
+
+    1. we calculate how many odds are there between 0 - high
+    2. we calculate the number of odds between 0- low
+    3. the difference between these two odds is the result
+    """
+
+    def countOdds(self, low: int, high: int) -> int:
+        hi_odds = high // 2 if high % 2 == 0 else (high + 1) // 2
+        lo_odds = low // 2 if low % 2 == 0 else (low - 1) // 2
+        return hi_odds - lo_odds
+
+
+def main():
+    low = 1
+    high = 13
+    odds = Solution().countOdds(low, high)
+    print(odds)
+
+
+if __name__ == '__main__':
+    main()
