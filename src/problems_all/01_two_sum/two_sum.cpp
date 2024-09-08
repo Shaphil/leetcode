2540305540f018b591a8071a68db21f249028737
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-09 03:08:43 +0600

    1. Two Sum - `C++` `Easy` - Add `Brute Force` solution

---
+++ src/problems_all/01_two_sum/two_sum.cpp
@@ -0,0 +1,46 @@
+/*
+Runtime:    152 ms
+Memory:     9.54 MB
+Submission: https://leetcode.com/problems/two-sum/submissions/211518943/
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int first, last;
+        int length = nums.size();
+        vector<int> v;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    first = i;
+                    last = j;
+                    break;
+                }
+            }
+        }
+        v.push_back(first);
+        v.push_back(last);
+
+        return v;
+    }
+};
+
+int main() {
+    int target = 9;
+    vector<int> nums {2, 7, 11, 15};
+    vector<int> result;
+    Solution solution;
+
+    result = solution.twoSum(nums, target);
+    for (auto item : result) {
+        cout << item << endl;
+    }
+
+    return 0;
+}
