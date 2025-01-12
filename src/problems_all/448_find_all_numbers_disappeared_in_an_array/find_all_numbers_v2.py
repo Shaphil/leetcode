47badccae59bde4d19ab9bfe73a1fb54e0ea094a
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-01-12 21:14:25 +0600

    448. Find All Numbers Disappeared in an Array - `v2` - `Python` - `easy` - `#hash-set`

---
+++ src/problems_all/448_find_all_numbers_disappeared_in_an_array/find_all_numbers_v2.py
@@ -0,0 +1,30 @@
+"""
+Runtime:            21 ms
+Beats:              81.49%
+Memory:             31.34 MB
+Beats:              33.33%
+Submission:         https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/submissions/1506278667/
+Time complexity:    O(n)
+Space complexity:   O(n)
+Topics:             #array, #hash-table
+Solved By:          #hash-set
+"""
+
+from typing import List
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = set(nums)
+        res = []
+        for i in range(1, len(nums) + 1):
+            if i not in n:
+                res.append(i)
+
+        return res
+
+
+if __name__ == '__main__':
+    nums = [4, 3, 2, 7, 8, 2, 3, 1]
+    result = Solution().findDisappearedNumbers(nums)
+    print(result)
